2ffdc2e82aed4f235ec42445fcc2ab57938cd480
Daniellebosmaa <35567543+Daniellebosmaa@users.noreply.github.com>
2018-01-18 14:56:55 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var x = document.getElementById("middle");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function descriptionFunction() {
+    var x = document.getElementById("description");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function reviewsFunction() {
+    var x = document.getElementById("reviews");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+var elem = document.getElementById('id');
